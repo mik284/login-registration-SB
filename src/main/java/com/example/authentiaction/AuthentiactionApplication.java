9698b95d4d68b00378da0dadd51240310e7ed525
mik284 <mikeykamau222@gmail.com>
2024-02-01 15:43:25 +0300

    initial commit auth-endpoint

---
+++ src/main/java/com/example/authentiaction/AuthentiactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.authentiaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentiactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentiactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authentiaction/AuthentiactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authentiaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentiactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
